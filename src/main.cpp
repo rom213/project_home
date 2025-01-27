78aa3d4ca161156a5457d37dad170f1db5962ae8
rom213 <romarioariza@outlook.es>
2025-01-26 21:10:50 -0500

    it was implement the funcionality that check status eternet forever, and it was implement alarm control with it can call when its activate the alarm

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,19 @@
 #include <ArduinoJson.h>
 #include <ESP32Servo.h>
 
-const char* ssid = "TP-Link_5648";
-const char* password = "12345678";
+const char *ssid = "TP-Link_5648";
+const char *password = "12345678";
 
 // Configuración del WebSocket
-const char* host = "192.168.1.104";
+const char *host = "192.168.1.108";
 const int port = 5000;
-const char* path = "/echo";
-
+const char *path = "/echo";
+const int port2 = 5001;
 WebSocketsClient webSocketClient;
 
 // Estructura para almacenar el estado de los botones
-struct ButtonState {
+struct ButtonState
+{
     bool security;
     bool opendoor;
     bool offlights;
@@ -26,11 +27,16 @@ struct ButtonState {
     bool bathroom;
     bool yarn;
     bool closeDoor;
+    bool ethernet;
+    bool ethernet_state;
 };
 
 // Inicializar el estado de los botones
-ButtonState buttonsState = {false, false, false, false, false, false, false, false, false};
+ButtonState buttonsState = {false, false, false, false, false, false, false, false, false, true, true};
 Servo servo;
+
+// Estatus del ethernet
+int status_ethernet = false;
 
 // Definir los pines GPIO para cada botón
 const int securityPin = 23;
@@ -42,10 +48,13 @@ const int dinningPin = 5;
 const int bathroomPin = 4;
 const int yarnPin = 16;
 const int closeDoorPin = 2;
-const int end_of_race_door= 34;
+const int end_of_race_door = 34;
+const int on_off_ethernet = 27;
+const int alarm_detected_pin = 14;
 
 // Función para configurar los pines GPIO
-void setupPins() {
+void setupPins()
+{
     pinMode(securityPin, OUTPUT);
     pinMode(opendoorPin, OUTPUT);
     pinMode(offlightsPin, OUTPUT);
@@ -55,31 +64,61 @@ void setupPins() {
     pinMode(bathroomPin, OUTPUT);
     pinMode(yarnPin, OUTPUT);
     pinMode(end_of_race_door, INPUT_PULLDOWN);
+    pinMode(on_off_ethernet, INPUT_PULLDOWN);
+    pinMode(alarm_detected_pin, INPUT_PULLDOWN);
+
     servo.attach(closeDoorPin, 500, 2500);
 }
 
-
 // Función para enviar el estado actualizado de los botones al servidor de Node.js
-void sendUpdatedState() {
-    DynamicJsonDocument doc(256);
-    doc["security"] = buttonsState.security;
-    doc["opendoor"] = buttonsState.opendoor;
-    doc["offlights"] = buttonsState.offlights;
-    doc["alarm"] = buttonsState.alarm;
-    doc["room"] = buttonsState.room;
-    doc["dinning"] = buttonsState.dinning;
-    doc["bathroom"] = buttonsState.bathroom;
-    doc["yarn"] = buttonsState.yarn;
-    doc["closeDoor"] = buttonsState.closeDoor;
+void sendUpdatedState()
+{
 
-    String output;
-    serializeJson(doc, output);
-    webSocketClient.sendTXT(output);
+    if (WiFi.status() == WL_CONNECTED && status_ethernet == true)
+    {
+        DynamicJsonDocument doc(256);
+        doc["security"] = buttonsState.security;
+        doc["opendoor"] = buttonsState.opendoor;
+        doc["offlights"] = buttonsState.offlights;
+        doc["alarm"] = buttonsState.alarm;
+        doc["room"] = buttonsState.room;
+        doc["dinning"] = buttonsState.dinning;
+        doc["bathroom"] = buttonsState.bathroom;
+        doc["yarn"] = buttonsState.yarn;
+        doc["closeDoor"] = buttonsState.closeDoor;
+        doc["ethernet"] = true;
+        doc["ethernet_state"] = true;
+
+        String output;
+        serializeJson(doc, output);
+        webSocketClient.sendTXT(output);
+    }
+
+    if (WiFi.status() == WL_CONNECTED && status_ethernet == false)
+    {
+        DynamicJsonDocument doc(256);
+        doc["security"] = buttonsState.security;
+        doc["opendoor"] = buttonsState.opendoor;
+        doc["offlights"] = buttonsState.offlights;
+        doc["alarm"] = buttonsState.alarm;
+        doc["room"] = buttonsState.room;
+        doc["dinning"] = buttonsState.dinning;
+        doc["bathroom"] = buttonsState.bathroom;
+        doc["yarn"] = buttonsState.yarn;
+        doc["closeDoor"] = buttonsState.closeDoor;
+        doc["ethernet"] = false;
+        doc["ethernet_state"] = true;
+
+        String output;
+        serializeJson(doc, output);
+        webSocketClient.sendTXT(output);
+    }
 }
 
 int check = 0;
 // Función para actualizar los pines GPIO según el estado de los botones
-void updatePins() {
+void updatePins()
+{
     digitalWrite(securityPin, buttonsState.security ? HIGH : LOW);
     digitalWrite(opendoorPin, buttonsState.opendoor ? HIGH : LOW);
     digitalWrite(offlightsPin, buttonsState.offlights ? HIGH : LOW);
@@ -89,43 +128,53 @@ void updatePins() {
     digitalWrite(bathroomPin, buttonsState.bathroom ? HIGH : LOW);
     digitalWrite(yarnPin, buttonsState.yarn ? HIGH : LOW);
 
-    int pinState= digitalRead(end_of_race_door);
-    
-    if (pinState==HIGH && check==0)
-    {
-        check=1;
-        buttonsState.closeDoor = false;
-        sendUpdatedState();
-        delay(200);
-    }
-    
+    int pinState = digitalRead(end_of_race_door);
 
-    if (pinState==LOW)
+    if (status_ethernet == true)
     {
-        check=0;
-        buttonsState.closeDoor = true;
-        sendUpdatedState();
-    }
-    
-    if (buttonsState.closeDoor && pinState==HIGH) {
-        check=0;
-        buttonsState.closeDoor = false;
-        sendUpdatedState();
-        servo.write(0);
-        delay(1500);
-        servo.write(180);
-        delay(1500);
+
+        if (pinState == HIGH && check == 0)
+        {
+            check = 1;
+            buttonsState.closeDoor = false;
+            sendUpdatedState();
+            delay(200);
+        }
+
+        if (pinState == LOW)
+        {
+            check = 0;
+            buttonsState.closeDoor = true;
+            sendUpdatedState();
+        }
+
+        if (buttonsState.closeDoor && pinState == HIGH)
+        {
+            check = 0;
+            Serial.println("cerrando puerta");
+            buttonsState.closeDoor = false;
+            sendUpdatedState();
+            servo.write(0);
+            delay(1500);
+            servo.write(180);
+            delay(1500);
+        }
     }
 }
 
 // Función de callback para recibir mensajes
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-    DynamicJsonDocument doc(256); // Declarar esto antes del switch statement
-    DeserializationError error; // Declarar esto antes del switch statement
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+    if (WiFi.status() == WL_CONNECTED && status_ethernet == true)
+    {
+        DynamicJsonDocument doc(256); // Declarar esto antes del switch statement
+        DeserializationError error;   // Declarar esto antes del switch statement
 
-    switch(type) {
+        switch (type)
+        {
         case WStype_DISCONNECTED:
             Serial.println("Desconectado!");
+
             break;
         case WStype_CONNECTED:
             Serial.println("Conectado!");
@@ -134,7 +183,8 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             Serial.printf("Mensaje recibido: %s\n", payload);
             // Parsear el JSON payload
             error = deserializeJson(doc, payload);
-            if (!error) {
+            if (!error)
+            {
                 // Actualizar el estado de los botones
                 buttonsState.security = doc["security"];
                 buttonsState.opendoor = doc["opendoor"];
@@ -147,59 +197,228 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
                 buttonsState.closeDoor = doc["closeDoor"];
 
                 // Imprimir el estado de cada botón
-                Serial.printf("security: %s\n", buttonsState.security ? "true" : "false");
-                Serial.printf("opendoor: %s\n", buttonsState.opendoor ? "true" : "false");
-                Serial.printf("offlights: %s\n", buttonsState.offlights ? "true" : "false");
-                Serial.printf("alarm: %s\n", buttonsState.alarm ? "true" : "false");
-                Serial.printf("room: %s\n", buttonsState.room ? "true" : "false");
-                Serial.printf("dinning: %s\n", buttonsState.dinning ? "true" : "false");
-                Serial.printf("bathroom: %s\n", buttonsState.bathroom ? "true" : "false");
-                Serial.printf("yarn: %s\n", buttonsState.yarn ? "true" : "false");
-                Serial.printf("closeDoor: %s\n", buttonsState.closeDoor ? "true" : "false");
+                // Serial.printf("security: %s\n", buttonsState.security ? "true" : "false");
+                // Serial.printf("opendoor: %s\n", buttonsState.opendoor ? "true" : "false");
+                // Serial.printf("offlights: %s\n", buttonsState.offlights ? "true" : "false");
+                // Serial.printf("alarm: %s\n", buttonsState.alarm ? "true" : "false");
+                // Serial.printf("room: %s\n", buttonsState.room ? "true" : "false");
+                // Serial.printf("dinning: %s\n", buttonsState.dinning ? "true" : "false");
+                // Serial.printf("bathroom: %s\n", buttonsState.bathroom ? "true" : "false");
+                // Serial.printf("yarn: %s\n", buttonsState.yarn ? "true" : "false");
+                // Serial.printf("closeDoor: %s\n", buttonsState.closeDoor ? "true" : "false");
                 // Actualizar los pines GPIO según el estado de los botones
                 updatePins();
-            } else {
+            }
+            else
+            {
                 Serial.println("Error parsing JSON!");
             }
             break;
         case WStype_BIN:
             Serial.println("Mensaje binario recibido");
             break;
+        }
+    }
+    if (WiFi.status() == WL_CONNECTED && status_ethernet == false)
+    {
+
+        DynamicJsonDocument doc(256); // Declarar esto antes del switch statement
+        DeserializationError error;   // Declarar esto antes del switch statement
+
+        switch (type)
+        {
+        case WStype_DISCONNECTED:
+            Serial.println("Desconectado!");
+
+            break;
+        case WStype_CONNECTED:
+            Serial.println("Conectado!");
+            break;
+        case WStype_TEXT:
+            Serial.printf("Mensaje recibido: %s\n", payload);
+            // Parsear el JSON payload
+            error = deserializeJson(doc, payload);
+            if (!error)
+            {
+                if (doc["ethernet"])
+                {
+                    Serial.println("estado cambiado");
+                    status_ethernet = true;
+
+                    updatePins();
+                }
+
+                // Actualizar el estado de los botones
+
+                // Imprimir el estado de cada botón
+                // Serial.printf("security: %s\n", buttonsState.security ? "true" : "false");
+                // Serial.printf("opendoor: %s\n", buttonsState.opendoor ? "true" : "false");
+                // Serial.printf("offlights: %s\n", buttonsState.offlights ? "true" : "false");
+                // Serial.printf("alarm: %s\n", buttonsState.alarm ? "true" : "false");
+                // Serial.printf("room: %s\n", buttonsState.room ? "true" : "false");
+                // Serial.printf("dinning: %s\n", buttonsState.dinning ? "true" : "false");
+                // Serial.printf("bathroom: %s\n", buttonsState.bathroom ? "true" : "false");
+                // Serial.printf("yarn: %s\n", buttonsState.yarn ? "true" : "false");
+                // Serial.printf("closeDoor: %s\n", buttonsState.closeDoor ? "true" : "false");
+                // Actualizar los pines GPIO según el estado de los botones
+            }
+            else
+            {
+                Serial.println("Error parsing JSON!");
+            }
+            break;
+        case WStype_BIN:
+            Serial.println("Mensaje binario recibido");
+            break;
+        }
     }
 }
 
-void connectToWiFi() {
-  Serial.print("Conectando a ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  
-  Serial.println("");
-  Serial.println("WiFi conectado");
-  Serial.println("Dirección IP: ");
-  Serial.println(WiFi.localIP());
+void connectToWiFi()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print("Conectando a ");
+        Serial.println(ssid);
+
+        WiFi.begin(ssid, password);
+
+        unsigned long tiempoInicio = millis();          // Marca de tiempo inicial
+        const unsigned long tiempoMaximoEspera = 10000; // Máximo tiempo de espera (10 segundos)
+
+        while (WiFi.status() != WL_CONNECTED)
+        {
+
+            if (millis() - tiempoInicio > tiempoMaximoEspera)
+            { // Si supera el tiempo máximo, salir
+                Serial.println("No se pudo conectar al WiFi dentro del tiempo límite.");
+                return; // Sale de la función si no se conecta dentro del tiempo
+            }
+
+            delay(500); // Pausa breve para evitar saturar el bucle
+            Serial.print(".");
+        }
+
+        // Si se conecta correctamente
+        Serial.println("\nWiFi conectado");
+        Serial.println("Dirección IP: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+void connectToWebSocket()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.print("Conectando a WebSocket en ");
+        Serial.println(host);
+
+        webSocketClient.begin(host, port, path);
+        webSocketClient.onEvent(webSocketEvent);
+    }
 }
 
-void connectToWebSocket() {
-  Serial.print("Conectando a WebSocket en ");
-  Serial.println(host);
-  
-  webSocketClient.begin(host, port, path);
-  webSocketClient.onEvent(webSocketEvent);
+void setup()
+{
+    Serial.begin(115200);
+    setupPins();
+    connectToWiFi();
+    connectToWebSocket();
 }
 
-void setup() {
-  Serial.begin(115200);
-  connectToWiFi();
-  connectToWebSocket();
-  setupPins();
+bool status_render = true;
+
+void ethernetControl()
+{
+    int pinStateEtherNet = digitalRead(on_off_ethernet);
+
+    if (pinStateEtherNet == HIGH)
+    {
+        Serial.println("estado cambiado desde el boton");
+        status_ethernet = !status_ethernet;
+        delay(500);
+    }
+
+    if (status_ethernet == true && status_render == true)
+    {
+        buttonsState = {false, false, false, false, false, false, false, false, false, false, true};
+        updatePins();
+        sendUpdatedState();
+        status_render = false;
+    }
+
+    if (status_ethernet == false && status_render == false)
+    {
+        buttonsState = {false, false, false, false, false, false, false, false, false, false, true};
+        updatePins();
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            sendUpdatedState();
+        }
+
+        status_render = true;
+    }
 }
 
-void loop() {
-  webSocketClient.loop();
+void checkEthernetDisconnect()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        static unsigned long lastTime = 0;
+        if (millis() - lastTime > 2000)
+        {
+            lastTime = millis();
+            webSocketClient.sendTXT("{\"heartbeat\": true}");
+        }
+    }
+}
+
+bool render_alarm = true;
+
+void checkAndSendAlarm()
+{
+    int pinAlarm = digitalRead(alarm_detected_pin);
+
+    if (pinAlarm == HIGH && render_alarm == true)
+    {
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            HTTPClient http;
+
+            // Construye la URL con parámetros (ajusta según tu backend)
+            String url = "http://192.168.1.108:5001/call_alarm";
+
+            http.begin(url);
+
+            int httpCode = http.GET();
+            http.end();
+            buttonsState.alarm = true;
+            sendUpdatedState();
+            render_alarm = false;
+        }
+        else
+        {
+            connectToWiFi();
+        }
+    }
+
+    if (pinAlarm == LOW && render_alarm == false)
+    {
+        buttonsState.alarm = false;
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            sendUpdatedState();
+        }
+
+        render_alarm = true;
+    }
+}
+void loop()
+{
+    webSocketClient.loop();
+
+    ethernetControl();
+
+    checkAndSendAlarm();
+
+    checkEthernetDisconnect();
 }
